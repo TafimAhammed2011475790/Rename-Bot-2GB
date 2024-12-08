c9dddeb241526af5e2a69d9e0b101e37d29da02a
Tafim Ahammed Tanvir <129662832+TafimAhammed2011475790@users.noreply.github.com>
2024-12-08 21:58:33 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,22 +5,22 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "27508013")
-    API_HASH  = os.environ.get("API_HASH", "c63c37c6451e33ca8e21a29707549687")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6772117091:AAE8haHPpjjGhsHq7B1ylfyZn2T2i_ExT7s") 
+    API_ID    = os.environ.get("API_ID", "")
+    API_HASH  = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","animesnetwork")     
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://animesnetwork:animesnetwork27@atlascluster.bek3qlm.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
+    DB_URL  = os.environ.get("DB_URL","")
  
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5904993061').split()]
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
     # channels logs
     FORCE_SUB   = os.environ.get("FORCE_SUB", "-1001656917510") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002397121816"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100239712181"))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
